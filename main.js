064a8a7abc322b8d377d588d9d2f3e0ae4733fc9
Brendan Hays <brendan@luggle.com>
2021-09-14 13:18:44 -0700

    Upgrade price text

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function increaseEarn() {
     if (gameData.money >= gameData.upgrade1Cost) {
         gameData.money -= gameData.upgrade1Cost
         gameData.upgrade1Cost *= upgradeRates.upgrade1
+        updateText()
     }
 }
 
@@ -26,6 +27,7 @@ var saveGameLoop = window.setInterval(function() {
 
 function updateText() {
     document.getElementById("moneyEarned").innerHTML = gameData.money + " money earned"
+    document.getElementById("upgrade1").innerHTML = "Upgrade price: " + gameData.upgrade1Cost
 }
 
 var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
